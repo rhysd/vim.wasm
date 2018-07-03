36a483d3732a45fa77cb44173dab48398ae5139f
rhysd <lin90162@yahoo.co.jp>
2018-07-04 02:19:46 +0900

    add preload script to mimic STDIN

---
+++ wasm/pre.js
@@ -0,0 +1,26 @@
+/* vi:set ts=4 sts=4 sw=4 et:
+ *
+ * VIM - Vi IMproved		by Bram Moolenaar
+ *				GUI/Motif support by Robert Webb
+ *	      Implemented by rhysd <https://github.com/rhysd>
+ *
+ * Do ":help uganda"  in Vim to read copying and usage conditions.
+ * Do ":help credits" in Vim to see a list of people who contributed.
+ * See README.txt for an overview of the Vim source code.
+ */
+
+/*
+ * runtime.js: preloaded JavaScript code for Wasm port of Vim by @rhysd.
+ */
+
+function stdin() {
+    // Tell that user did not give any input. GUI input is caught in 'keydown' listener.
+    return null;
+}
+
+function prerun() {
+    console.log('prerun');
+    FS.init(stdin, null, null);
+}
+
+Module.preRun.push(prerun);
