74ff46c3604af6cf5f40886a0aa75cf62c1de44e
rhysd <lin90162@yahoo.co.jp>
2019-06-21 07:02:44 +0900

    release object URL after download

--- wasm/main.ts
+++ wasm/main.ts
@@ -818,10 +818,12 @@ vim.onFileExport = (fullpath: string, contents: ArrayBuffer) => {
     const a = document.createElement('a');
     a.style.display = 'none';
     a.href = url;
+    a.rel = 'noopener';
     a.download = filename;
     document.body.appendChild(a);
     a.click();
     document.body.removeChild(a);
+    URL.revokeObjectURL(url);
 };
 
 vim.start({ debug: debugging, perf });
