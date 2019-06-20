3b5fe09c56b37049a2331702e8c968e732ea9794
rhysd <lin90162@yahoo.co.jp>
2019-06-20 18:43:37 +0900

    expose callback for file exports

--- wasm/main.ts
+++ wasm/main.ts
@@ -543,6 +543,7 @@ interface StartOptions {
 class VimWasm {
     public onVimInit?: () => void;
     public onVimExit?: (status: number) => void;
+    public onFileExport?: (fullpath: string, contents: ArrayBuffer) => void;
     private readonly worker: VimWorker;
     private readonly screen: ScreenCanvas;
     private readonly resizer: ResizeHandler;
@@ -652,7 +653,17 @@ class VimWasm {
                 debug('draw event', msg.event);
                 break;
             case 'export':
-                this.exportFile(msg.path, msg.contents);
+                debug(
+                    'Exporting file',
+                    msg.path,
+                    'with size',
+                    msg.contents.byteLength,
+                    'bytes with',
+                    this.onFileExport,
+                );
+                if (this.onFileExport !== undefined) {
+                    this.onFileExport(msg.path, msg.contents);
+                }
                 break;
             case 'started':
                 this.screen.onVimInit();
@@ -683,20 +694,6 @@ class VimWasm {
             default:
                 throw new Error(`FATAL: Unexpected message from worker: ${JSON.stringify(msg)}`);
         }
-    }
-
-    private exportFile(fullpath: string, contents: ArrayBuffer) {
-        const slashIdx = fullpath.lastIndexOf('/');
-        const filename = slashIdx !== -1 ? fullpath.slice(slashIdx + 1) : fullpath;
-        const blob = new Blob([contents], { type: 'application/octet-stream' });
-        const url = URL.createObjectURL(blob);
-        const a = document.createElement('a');
-        a.style.display = 'none';
-        a.href = url;
-        a.download = filename;
-        document.body.appendChild(a);
-        a.click();
-        document.body.removeChild(a);
     }
 
     private printPerfs() {
@@ -811,5 +808,19 @@ if (!perf) {
         alert(`Vim exited with status ${status}`);
     };
 }
+
+vim.onFileExport = (fullpath: string, contents: ArrayBuffer) => {
+    const slashIdx = fullpath.lastIndexOf('/');
+    const filename = slashIdx !== -1 ? fullpath.slice(slashIdx + 1) : fullpath;
+    const blob = new Blob([contents], { type: 'application/octet-stream' });
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.style.display = 'none';
+    a.href = url;
+    a.download = filename;
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+};
 
 vim.start({ debug: debugging, perf });
