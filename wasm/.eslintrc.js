c70b89be994ca746dcc5e95a45f5f7811ab8f5e3
rhysd <lin90162@yahoo.co.jp>
2019-07-03 00:11:27 +0900

    disable eslint run which bans ///

--- wasm/.eslintrc.js
+++ wasm/.eslintrc.js
@@ -37,6 +37,7 @@ module.exports = {
         '@typescript-eslint/no-explicit-any': 'off',
         '@typescript-eslint/camelcase': 'off', // Due to vimwasm_* C APIs
         '@typescript-eslint/explicit-member-accessibility': 'off',
+        '@typescript-eslint/no-triple-slash-reference': 'off', // For common.d.ts, /// <reference/> is necessary
         'security/detect-non-literal-fs-filename': 'off',
         'security/detect-object-injection': 'off', // false positive at array index accesses
 
