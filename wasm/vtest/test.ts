ca23ae4649b176b147697ba52f8557b04a7178ee
rhysd <lin90162@yahoo.co.jp>
2019-07-28 14:21:55 +0900

    do not store expected screenshot image for visual testing
    since font is different among platforms, it is not possible to use one
    screenshot as correct data.

--- wasm/vtest/test.ts
+++ wasm/vtest/test.ts
@@ -1,12 +1,25 @@
 import * as path from 'path';
+import { promises as fs } from 'fs';
 import puppeteer = require('puppeteer');
 import { createServer } from 'http-server';
 import { imgDiff } from 'img-diff-js';
+import open = require('open');
 
-const SCREENSHOT_PATH = path.join(__dirname, 'actual', 'screenshot.png');
+const SCREENSHOT_PATH = path.join(__dirname, 'actual.png');
+const DIFF_PATH = path.join(__dirname, 'diff.png');
+const EXPECTED_PATH = path.join(__dirname, 'expected.png');
 
 function wait(ms: number) {
     return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function fileExists(file: string) {
+    try {
+        const s = await fs.stat(file);
+        return s.isFile();
+    } catch (_) {
+        return false;
+    }
 }
 
 function server(port: number) {
@@ -38,13 +51,11 @@ async function capture(port: number) {
     }
 }
 
-async function diff() {
-    const diffFile = path.join(__dirname, 'diff.png');
-    const expectedFile = path.join(__dirname, 'expected', 'screenshot.png');
+async function check() {
     const result = await imgDiff({
         actualFilename: SCREENSHOT_PATH,
-        expectedFilename: expectedFile,
-        diffFilename: diffFile,
+        expectedFilename: EXPECTED_PATH,
+        diffFilename: DIFF_PATH,
         threshold: 0,
     });
 
@@ -54,22 +65,36 @@ async function diff() {
     }
 
     console.error(`FAIL: ${result.diffCount} diffs found in ${result.width}x${result.height} image`);
-    console.error(`  - See ${diffFile} for the diffs`);
+    console.error(`  - See ${DIFF_PATH} for the diffs`);
     console.error(`  - See ${SCREENSHOT_PATH} for the actual screenshot`);
-    console.error(`  - See ${expectedFile} for the expected screenshot`);
+    console.error(`  - See ${EXPECTED_PATH} for the expected screenshot`);
     return 1;
+}
+
+async function firstTime() {
+    await fs.copyFile(SCREENSHOT_PATH, EXPECTED_PATH);
+    console.error('FAIL: Expected screenshot does not exist');
+    console.error(`Please check a current screenshot ${SCREENSHOT_PATH} is fine manually`);
+    console.error(`Since screenshot was copied to ${EXPECTED_PATH}, please remove it if it is not expected`);
+    await open(SCREENSHOT_PATH);
 }
 
 async function main() {
     const port = 5963;
     const s = server(port);
+
     try {
         await capture(port);
     } finally {
         s.close();
     }
-    const code = await diff();
-    process.exit(code);
+
+    if (!(await fileExists(EXPECTED_PATH))) {
+        await firstTime();
+        process.exit(1);
+    }
+
+    process.exit(await check());
 }
 
 main().catch(e => {
